230f874b5fcf004f28a4e6e653e88a51d08bfa70
Arnary <anastasia.dushka1@gmail.com>
2024-06-15 13:03:27 +0300

    minor fix

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,11 +1,12 @@
-import axios from "axios";
 
-
-axios.defaults.headers.common["x-api-key"] = "live_3CKy5yTuXrAzCmE7H7xrCVB77JtRAYCYWX8J4u5nAGZO565MKxw9oOx7YjZ5E4Cq";
+const ApiKey = "live_3CKy5yTuXrAzCmE7H7xrCVB77JtRAYCYWX8J4u5nAGZO565MKxw9oOx7YjZ5E4Cq";
 const breedsUrl = "https://api.thecatapi.com/v1/breeds";
 
 export function fetchBreeds() {
-  return fetch(breedsUrl).then((response) => {
+  return fetch(breedsUrl, {headers: {
+      'x-api-key': ApiKey,
+    }}).then((response) => {
+    console.log(response)
     if (!response.ok) {
         return;
     }
@@ -16,7 +17,9 @@ export function fetchBreeds() {
 const ImgUrl = "https://api.thecatapi.com/v1/images/search";
 
 export function fetchCatByBreed(breedId) {
-    return fetch(`${ImgUrl}?breed_ids=${breedId}`).then((response) => {
+    return fetch(`${ImgUrl}?breed_ids=${breedId}`, {headers: {
+      'x-api-key': ApiKey,
+    }}).then((response) => {
         if (!response.ok) {
             return;
         }
